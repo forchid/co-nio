79ae30be1608e70c4c2cb8bb49c2bfcd68b0d7a0
Uncle-pan <jnimbus@163.com>
2018-08-20 19:45:06 +0800

    Add LICENSE file and copyright notices

--- src/test/java/io/conio/ScheduledHandler.java
+++ src/test/java/io/conio/ScheduledHandler.java
@@ -1,12 +1,25 @@
+/*
+ * Copyright (c) 2018, little-pan, All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ */
 package io.conio;
 
 import com.offbynull.coroutines.user.Continuation;
-import io.conio.util.CoFuture;
 import io.conio.util.ScheduledCoFuture;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.concurrent.ExecutionException;
 
 public class ScheduledHandler extends BaseTest implements CoHandler {
 
